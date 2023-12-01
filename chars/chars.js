7ba057475d0fcc6f76771cd69e3c962fa41a6249
Adrian <114053643+AdrianPrzybylskiTIE9@users.noreply.github.com>
2023-12-01 12:27:09 +0100

    Add joi validation

--- chars/chars.js
+++ chars/chars.js
@@ -1,17 +1,29 @@
 const fs = require("fs");
 const path = require("path");
+const joi = require("joi");
 
 let chars = require("./chars.json");
 
+const charSchema = joi.object({
+  id: joi.number().required(),
+  nazwa: joi.string().required(),
+  opis: joi.string().required(),
+  cena: joi.number().min(0).required(),
+})
+
 const addChar = (charData) => {
+  const { error } = charSchema.validate(charData);
+
+  if (error) {
+    throw new Error(`Validation error: ${error.details.map((detail) => detail.message).join(', ')}`);
+  }
+
   charData.id = getNextId();
-
   chars.push(charData);
-
   save();
-
   return getCharacter(charData.id);
 };
+
 
 const copy = (obj) => JSON.parse(JSON.stringify(obj));
 
@@ -38,11 +50,27 @@ const save = () => {
 };
 
 const updateChar = (charData) => {
+  const { error } = joi.object({
+    id: joi.number().integer().min(1).required(),
+    nazwa: joi.string().required(),
+    opis: joi.string().required(),
+    cena: joi.number().min(0).required(),
+  }).validate(charData);
+
+  if (error) {
+    throw new Error(`Validation error: ${error.details.map((detail) => detail.message).join(', ')}`);
+  }
+
   let char = findCharById(charData.id);
+  if (!char) {
+    throw new Error(`Character with ID ${charData.id} not found.`);
+  }
+
   Object.assign(char, charData);
   save();
   return getCharacter(char.id);
 };
+
 
 const deleteChar = (id) => {
     console.log(`delte id ${id}`);
